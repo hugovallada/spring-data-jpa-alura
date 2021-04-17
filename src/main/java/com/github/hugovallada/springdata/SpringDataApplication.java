c14a563f76be9b540664df02accba214d2009147
hugovallada <valladahugo@gmail.com>
2021-04-17 10:58:14 -0300

    Injeta um CargoRepository e salva um Cargo no banco

--- src/main/java/com/github/hugovallada/springdata/SpringDataApplication.java
+++ src/main/java/com/github/hugovallada/springdata/SpringDataApplication.java
@@ -1,13 +1,32 @@
 package com.github.hugovallada.springdata;
 
+import com.github.hugovallada.springdata.entities.Cargo;
+import com.github.hugovallada.springdata.repositories.CargoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
+
+
+	private final CargoRepository cargoRepository;
+
+	@Autowired
+	public SpringDataApplication(CargoRepository cargoRepository){
+		this.cargoRepository = cargoRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("Desenvolvedor de Software");
+
+		cargoRepository.save(cargo);
+	}
 }
