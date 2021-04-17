c92cf0c51c128dd99573e42860d05431a3d29ded
hugovallada <valladahugo@gmail.com>
2021-04-17 10:45:45 -0300

    Adiciona um CargoRepository que estende de CrudRepository

---
+++ src/main/java/com/github/hugovallada/springdata/repositories/CargoRepository.java
@@ -0,0 +1,7 @@
+package com.github.hugovallada.springdata.repositories;
+
+import com.github.hugovallada.springdata.entities.Cargo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CargoRepository extends CrudRepository<Cargo, Long> {
+}
