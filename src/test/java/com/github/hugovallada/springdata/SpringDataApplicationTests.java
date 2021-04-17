d9e270d430347fdf8ae0462e7473da1bb0a34a83
hugovallada <valladahugo@gmail.com>
2021-04-17 10:35:43 -0300

    Projeto inicial do Curso de Spring Data JPA da Alura

---
+++ src/test/java/com/github/hugovallada/springdata/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hugovallada.springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
